96f44a4c0cd540fe7fdd4ea3c99858b2b5256995
cesc <victorcesconetto@gmail.com>
2022-04-13 16:13:10 -0300

    Read modify update

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-// main.cpp - aula 1
 #include <stdio.h>
 
 // LED - PB5 - Arduino 13
@@ -6,18 +5,27 @@
 
 //to compile : avr-gcc -mmcu=atmega328p file -o output
 
-int led_pin = 5;
 
+//olhar na imagem do datasheet
+int led_pin = 5;// pinb5 = port13 arduino
+int bot_pin = 2;// pind2 = port2 arduino
+
+//port b 0x24 0x23 0x25
 unsigned char *ddrb = (unsigned char *) 0x24;//0x24 endereco de memoria
 unsigned char *pinb = (unsigned char *) 0x23;
 unsigned char *portb = (unsigned char *) 0x25;
 
+//port d - 0x29 0x2a 0x2b
+unsigned char *ddrd = (unsigned char *) 0x29;//0x24 endereco de memoria
+unsigned char *pind = (unsigned char *) 0x2a;
+unsigned char *portd = (unsigned char *) 0x2b;
+
+
 
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-//   pinMode(LED_BUILTIN, OUTPUT);
-    *ddrb = ( 1 << led_pin );  // 0010 0000
-   
+
+    *ddrb = ( 1 << led_pin );  // 0010 0000 correspondente ao shift de 5 no led 
+    *ddrd = ((*ddrd) & ~( 1 << bot_pin ));  // 1111 1011 correspondente ao shift de 2 do botao
 }
 
 void delay1000(){
