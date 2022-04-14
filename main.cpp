c29b53af346349017fb2519ed166f5b863096425
Victor <victorcesconetto@gmail.com>
2022-04-14 16:27:32 -0300

    Usando interrupçoes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
-#include <stdio.h>
-
+#include <avr/interrupt.h>
 // LED - PB5 - Arduino 13
 // BTN - PD2 - Arduino 2
 
@@ -9,6 +8,8 @@
 //olhar na imagem do datasheet
 int led_pin = 5;// pinb5 = port13 arduino
 int bot_pin = 2;// pind2 = port2 arduino
+volatile int bot = 0 ; // sempre ser buscada em memoria, nunca ser permanentemente salva no registrador
+
 
 //port b 0x24 0x23 0x25
 unsigned char *ddrb = (unsigned char *) 0x24;//0x24 endereco de memoria
@@ -20,13 +21,27 @@ unsigned char *ddrd = (unsigned char *) 0x29;//0x24 endereco de memoria
 unsigned char *pind = (unsigned char *) 0x2a;
 unsigned char *portd = (unsigned char *) 0x2b;
 
+//interrupc
+
+unsigned char *eicra = (unsigned char *) 0x69;
+unsigned char *eimsk = (unsigned char *) 0x3d;
 
 
 void setup() {
 
     *ddrb = ((*ddrb) | ( 1 << led_pin ));  // 0010 0000 correspondente ao shift de 5 no led 
     *ddrd = ((*ddrd) & ~( 1 << bot_pin ));  // 1111 1011 correspondente ao shift de 2 do botao
+    *eicra = ( (*eicra) & 0xf0 | 0x03 ); // (*eicra) & 0xf0 == 1111 0000 
+    // zera os quatro da direita e escreve 1111 nos da esq, apos isso faz um | com os dados a serem atribuidos
+    *eimsk = ( (*eimsk) & 0xfc | 0x01 );
+    sei();
+}   // 0xfc = 1111 1100
+
+//ISR(ADC_vect) // nome na tabela do libc
+ISR(INT0_vect){
+  bot = !bot;
 }
+
 
 void delay1000(){
   unsigned long x = 0x7ffff;
@@ -34,7 +49,7 @@ void delay1000(){
 } 
 
 void ledOn(){
-    *portb = *portb | (1 << led_pin);//mantem os bits da porta e apenas altera o especifico - vale pra todos
+    *portb = *portb | (1 << led_pin);//mantem os bits da porta e apenas altera o especifico - vale pra todos ^
 }
 
 
@@ -44,7 +59,8 @@ void ledOff(){
 
 bool botao(){
   // ler o valor do botao pind
-    return ( (*pind) & (1 << bot_pin) ) > 0;
+  //  return ( (*pind) & (1 << bot_pin) ) > 0;
+  return bot;
   // return botao esta on ou off?
 }
 
