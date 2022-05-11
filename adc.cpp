6c3bb39d453dca69fb983cf8d264269f115ee63b
victor <haha>
2022-05-11 17:18:02 -0300

    teste com double atod converter pra ver a Tensao do potenciometro

--- adc.cpp
+++ adc.cpp
@@ -1,23 +1,24 @@
 #include "adc.h"
 #include <avr/io.h>
 
-
-
-AdcChannel::AdcChannel(int ch) : channel(ch){
-    ADMUX = (1 << REFS0) | (0x0f  & channel);
-    ADCSRA  = (1<< ADEN);
+AdcChannel::AdcChannel(int ch)
+    : channel(ch)
+{
+    ADMUX = (1 << REFS0) | (0x0f & channel);
+    ADCSRA = (1 << ADEN) | (6 << ADPS0);
+    ADCSRB = 0;
 }
 
-int AdcChannel::get(){
-    
-    ADMUX = (ADMUX & 0Xf0) | (0x0f & channel);
-    ADCSRA |= (1 << ADSC);
+int AdcChannel::get()
+{
+    ADMUX = (ADMUX & 0xf0) | (0x0f & channel);
 
-    while (ADCSRA & (1 << ADSC));
-    
     ADCSRA |= (1 << ADSC);
+    while (ADCSRA & (1 << ADSC))
+        ;
+    ADCSRA |= (1 << ADSC);
+    while (ADCSRA & (1 << ADSC))
+        ;
 
-    while (ADCSRA & (1 << ADSC));    
     return ADC;
-
 }
--- main.cpp
+++ main.cpp
@@ -12,10 +12,41 @@
 Uart uart(9600);
 //port b 0x24 0x23 0x25
 AdcChannel adc0(0);
+//mapear led?
+int potMax = 0;
+int potMin = 1023;
+
+// void calibratePot(){
+//   uart.puts("Calibrando...\n");
+//   for(int i=0;i<1000;i++){
+//   int pot = analogRead(A0);  
+//   if(pot < potMin){
+//       potMin = pot;
+//   }
+//   if(pot > potMax){
+//       potMax = pot; 
+//   }
+  
+//   delay(10);
+//   }
+//   uart.puts("Calibrado!!\n");
+// }
+
+
+int map_led = 255;
+int Pmap(int pot){
+    int range = (int) pot - potMin + 1;
+    int teste = range/map_led;
+    int result = pot/teste;
+    if(result < 0) return 0;
+    else if (result > map_led) return map_led;
+    else return 0;        
+}
 
 
 void setup() {
   uart.puts("Setup\n");
+  //calibratePot();
 }   // 0xfc = 1111 1100
 
 
@@ -24,14 +55,18 @@ void delay1000(){
   while(x--);  
 } 
 
+int mVcc = 5000;
 void readAdc0(){
   int val = adc0.get();
+  int out = val*((double)mVcc/1023);
   char buffer[10];
-  sprintf(buffer,"%d\n", val);
+  sprintf(buffer,"milivolts  %d\n", out);
+  //sprintf(buffer,"Digital : %d\n", val);
   uart.puts(buffer);
 }
 // the loop function runs over and over again forever
-void loop() {    
+void loop() {
+
     readAdc0();
     delay1000();
 }
